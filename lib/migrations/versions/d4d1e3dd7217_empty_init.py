4a1981524f5fb79a9b83c6f9a961399c32544ac1
Alexander <alex.galev@yahoo.com>
2023-03-27 08:30:11 -0600

    solved

---
+++ lib/migrations/versions/d4d1e3dd7217_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: d4d1e3dd7217
+Revises: 
+Create Date: 2023-03-27 08:16:00.532544
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd4d1e3dd7217'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
